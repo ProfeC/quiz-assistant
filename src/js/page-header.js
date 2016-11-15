d94d71406e25d41c2d455e7c27835d18b556e0d2
G. Lee Clark, II <gclark2@gmail.com>
2016-11-14 21:17:26 -0500

    Adds title to page header

--- src/js/page-header.js
+++ src/js/page-header.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class PageHeader extends React.Component {
+	constructor(props) {
+		super(props);
+
+		// console.info( "\nMounted: 'Current Word' Component." );
+	}
+
 	render() {
-		return ( <header><h1>Spelling Quiz Assistant</h1></header>);
+		return (
+			<header>
+				<h3><em>"{ this.props.title }"</em> Spelling Quiz Assistant</h3>
+			</header>
+		);
 	}
 }
 
