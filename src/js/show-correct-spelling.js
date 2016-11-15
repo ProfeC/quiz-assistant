0f2aa9786052dfd4dbb96627e0cc86d274c1bca1
G. Lee Clark, II <gclark2@gmail.com>
2016-11-14 21:19:49 -0500

    Updates imported components

---
+++ src/js/show-correct-spelling.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class ShowCorrectSpelling extends React.Component {
+	constructor(props) {
+		super(props);
+
+		// console.info( "\nMounted: 'Correct Spelling' Component." );
+	}
+
+	render() {
+		const showMe = this.props.show;
+		const checkMe = this.props.checked;
+		// console.log({showMe});
+		// console.log({checkMe});
+
+		if ( !this.props.checked ) {
+			return (
+				null
+			);
+		}
+
+		if ( this.props.checked && !this.props.show ) {
+			return (
+				<h2 className="incorrect">Incorrect!</h2>
+			);
+		}
+
+		return (
+			<div>
+				<h2 className="correct">Correct!</h2>
+			</div>
+		);
+	}
+}
+
+export default ShowCorrectSpelling;
--- src/js/show-current-word.js
+++ src/js/show-current-word.js
@@ -4,46 +4,14 @@ class ShowCurrentWord extends React.Component {
 	constructor( props ) {
 		super( props );
 
-		this.state = {
-			showWord: true
-		};
-	}
-
-	componentWillMount() {
-		console.log( "Mounted ShowCurrentWord" );
-		this.displayWord();
-	}
-
-	componentWillUnmount() {
-		console.log( "Unmounted ShowCurrentWord" );
-		clearInterval( this.timerID );
-	}
-
-	startTimer() {
-		this.timerID = setInterval(
-			() => this.hideWord(),
-			5000
-		);
-	}
-
-	hideWord() {
-		console.log("timer's up!");
-		clearInterval( this.timerID );
-		this.setState({showWord: false});
-	}
-
-	displayWord() {
-		this.startTimer();
-		this.setState({showWord: true});
-		console.log("timer started!");
+		// console.info( "\nMounted: 'Current Word' Component." );
 	}
 
 	render() {
-		var wordStyle;
-		wordStyle = this.state.showWord ? 'visible' : 'hidden';
-		console.log("Word Style: " + wordStyle);
+		const wordStyle = this.props.visibility ? 'visible' : 'hidden';
+		console.info("\nWord Style: " + wordStyle);
 
-		return ( <h2 id="current-word" style={{visibility: wordStyle}}>{ this.props.word }</h2>);
+		return (<h1 id="current-word" style={{ visibility: wordStyle }}>{ this.props.word }</h1>);
 	}
 }
 
