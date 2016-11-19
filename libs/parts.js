228fe9a0e5052131474e86e9a01db129adb48c5c
G. Lee Clark, II <gclark2@gmail.com>
2016-11-18 21:19:48 -0500

    React configured
    Followed along this path: http://survivejs.com/webpack/advanced-techniques/configuring-react/. However not all steps were included as errors were thrown.

--- libs/parts.js
+++ libs/parts.js
@@ -179,3 +179,4 @@ exports.clean = function(path) {
 // 		]
 // 	};
 // }
+
--- src/js/container.js
+++ src/js/container.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   var container = document.createElement('div')
   container.id = 'root'
-  container.innerHTML = 'Loading...'
+  container.innerHTML = 'Loading. Please wait...'
 
   return container
 }
