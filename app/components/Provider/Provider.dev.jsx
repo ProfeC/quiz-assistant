b599232b9b84b8c1325735bd3b23c660c49850b7
G. Lee Clark, II <gclark2@gmail.com>
2016-11-25 13:19:22 -0500

    Adds Chrome Debugging.
    ... kinda.

--- app/components/Provider/Provider.dev.jsx
+++ app/components/Provider/Provider.dev.jsx
@@ -1,12 +1,15 @@
 // REF: http://survivejs.com/react/implementing-kanban/react-and-flux/#setting-up-a-provider-
 import React from 'react';
 import AltContainer from 'alt-container';
+import chromeDebug from 'alt-utils/lib/chromeDebug';
 import alt from '../../libs/alt';
 import setup from './setup';
 
 setup(alt);
 
-// React.Perf = require('react-addons-perf');
+chromeDebug(alt);
+
+React.Perf = require('react-addons-perf');
 
 export default ({children}) =>
   <AltContainer flux={alt}>
