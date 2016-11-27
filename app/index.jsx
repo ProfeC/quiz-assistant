6ee325db068294d9bb5368480983fa811c7fb323
G. Lee Clark, II <gclark2@gmail.com>
2016-11-27 09:56:22 -0500

    Rearranged imports.

--- app/index.jsx
+++ app/index.jsx
@@ -7,16 +7,15 @@ document.body.prepend(container)
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-// import App from './components/app'
-import Words from './components/words'
 // import Provider from './components/Provider'
 
+// import App from './components/app'
 // ReactDOM.render(
 //   <Provider><App /></Provider>,
 //   document.getElementById('root')
 // )
 
+import Words from './components/words'
 ReactDOM.render(
   <Words />,
   document.getElementById('root')
