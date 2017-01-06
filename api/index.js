a693833554474992f74b9dd782bb00bace2c5149
G. L. Clark, II <gclark2@gmail.com>
2017-01-06 11:57:53 -0500

    Adds API endpoint for word list

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import * as Utils from '../app/libs/utils';
 
 const router = express.Router();
+let wordList = [];
 
-router.get('/', (req, res) => {
-	res.send({ data: [] });
+router.get('/words/:listID', (req, res) => {
+    // res.send(req.params);
+
+    wordList = Utils.getList(req.params.listID)
+
+	res.send({ 'wordList': wordList });
 });
 
 export default router;
