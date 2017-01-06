c6b4ea672c23cbc77a0e98130a3d03f3aa4c5edb
G. Lee Clark, II <gclark2@gmail.com>
2017-01-05 23:07:04 -0500

    Removes extra code.

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,3 @@
-// var container = require(PATH.components + 'container')
-  var container = document.createElement('div')
-  container.id = 'root'
-  container.innerHTML = 'Loading. Please wait...'
-
-document.body.prepend(container)
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
