9b60f74cc4e7a69affbcd2980c6f007ea125c8a2
G. Lee Clark, II <gclark2@gmail.com>
2017-01-09 19:49:13 -0500

    Adds new list of words.
    Also rearranges files in decending order.

--- app/components/data.js
+++ app/components/data.js
@@ -3,11 +3,12 @@ import React from 'react'
 
 class WordList extends React.Component {
 
-  const data_20161024 = require('../data/20161024.json')
-  const data_20161031 = require('../data/20161031.json')
-  const data_20161107 = require('../data/20161107.json')
-  const data_20161121 = require('../data/20161121.json')
+  const data_20170109 = require('../data/20170109.json')
   const data_20161127 = require('../data/20161127.json')
+  const data_20161121 = require('../data/20161121.json')
+  const data_20161107 = require('../data/20161107.json')
+  const data_20161031 = require('../data/20161031.json')
+  const data_20161024 = require('../data/20161024.json')
 
   render () {
     return (
