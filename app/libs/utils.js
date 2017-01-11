c9f902b25fd2d1838033aa0ea5858346d04b9241
G. L. Clark, II <gclark2@gmail.com>
2017-01-11 15:32:55 -0500

    Renames file-list to navigation

--- app/libs/utils.js
+++ app/libs/utils.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 // NOTE: Require data files.
 // FIX: THERE MUST BE A BETTER WAY TO DO THIS!
-const data_fileList = require('../data/file-list.json')
+const data_navigation = require('../data/navigation.json')
 const data_20161024 = require('../data/20161024.json')
 const data_20161031 = require('../data/20161031.json')
 const data_20161107 = require('../data/20161107.json')
