d8265be7b2fe97bd24fe232c93bff6aa77e7492b
G. L. Clark, II <gclark2@gmail.com>
2017-01-24 16:04:23 -0500

    Comments unused route.

--- server.js
+++ server.js
@@ -6,47 +6,32 @@ import axios from 'axios';
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from './app/components/app'
-// import Words from './app/components/words'
 
 const server = express();
 
 server.set('view engine', 'ejs');
 
-// server.get('/test/:quizID', (req, res) => {
-//     console.info(req.params)
-//     console.info(res.data)
+// server.get('/quiz/:quizID', (req, res) => {
+//     // NOTE: Get initial quiz data
+//     axios.get(`${config.serverUrl}/api/quiz/${req.params.quizID}`)
+//     .then(resp => {
+//         // console.info(resp.data) // Returns Word list.
 //
-//     serverRender(req.params.quizID, 3)
-//     .then( ({ initialMarkup, initialData }) => {
-//         console.info(initialData)
-//
-//         res.render({
-//             initialMarkup,
-//             initialData
+//         res.render('index', {
+//             initialMarkup: ReactDOMServer.renderToString(
+//             <App quizID={req.params.quizID} displayTime='13' initialData={[]} />),
+//             quizID: `${req.params.quizID}`,
+//             initialData: {data: resp.data, currentQuizID: `${req.params.quizID}`},
+//             // initialData: resp.data,
+//             avaiableQuizzes: []
 //         });
-//
 //     })
-// })
+//     .catch(console.error);
+// });
 
-server.get('/quiz/:quizID', (req, res) => {
-    // NOTE: Get initial quiz data
-    axios.get(`${config.serverUrl}/api/quiz/${req.params.quizID}`)
-    .then(resp => {
-        // console.info(resp.data) // Returns Word list.
+server.get(['/', '/quiz/:quizID'], (req, res) => {
+    console.info('req.params.quizID is ' + req.params.quizID)
 
-        res.render('index', {
-            initialMarkup: ReactDOMServer.renderToString(
-            <App quizID={req.params.quizID} displayTime='13' initialData={[]} />),
-            quizID: `${req.params.quizID}`,
-            initialData: {data: resp.data, currentQuizID: `${req.params.quizID}`},
-            // initialData: resp.data,
-            avaiableQuizzes: []
-        });
-    })
-    .catch(console.error);
-});
-
-server.get('/', (req, res) => {
     serverRender(req.params.quizID)
         .then(({initialMarkup, initialData}) => {
             // console.info('server => serverRender(req.params.quizID) => initialMarkup is ' + initialMarkup)
@@ -58,19 +43,6 @@ server.get('/', (req, res) => {
             });
         })
         .catch(console.error)
-
-    // axios.get(`${config.serverUrl}/api/quizzes`)
-    // .then(resp => {
-    //     console.info(resp.data.quizzes)
-    //
-    //     res.render('index', {
-    //         initialMarkup: ReactDOMServer.renderToString(
-    //         <App initialData={resp.data.quizzes} />),
-    //         initialData: resp.data.quizzes,
-    //         quizID: 0
-    //     });
-    // })
-    // .catch(console.error);
 });
 
 server.use('/api', apiRouter);
