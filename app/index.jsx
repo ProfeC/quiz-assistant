b306fcd8cc0faf8b11b6342f33aed22b3c8c1bf7
G. L. Clark, II <gclark2@gmail.com>
2017-01-24 12:23:52 -0500

    Removes commented or moved code.

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Utils from './libs/utils'
-
 import App from './components/app'
-import Words from './components/words'
-import PageHeader from './components/page-header'
-import PageFooter from './components/page-footer'
-
-ReactDOM.render(
-    <PageHeader title='Quiz Assistant - Main Application' skill='' />,
-  document.getElementById('header-main')
-)
 
 ReactDOM.render(
     <App quizzes={window.avaiableQuizzes} quizID={window.quizID} />,
   document.getElementById('root')
 )
-
-// ReactDOM.render(
-//     <Words quizID={Utils.getUrlParam('list') || '20161127'} displayTime={Utils.getUrlParam('displayTime')} />,
-//   document.getElementById('root')
-// )
