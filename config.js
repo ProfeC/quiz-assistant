f4bd30730e56e21082a119127c262ed3a105cc11
G. L. Clark, II <gclark2@gmail.com>
2017-01-27 15:58:14 -0500

    Adds MongoDB to project

--- config.js
+++ config.js
@@ -3,17 +3,18 @@ const env = process.env;
 export const nodeEnv = env.NODE_ENV || 'development';
 
 export const logStars = function(message) {
-  console.info('**********');
-  console.info(message);
-  console.info('**********');
+    console.info('**********');
+    console.info(message);
+    console.info('**********');
 };
 
 export default {
-  port: env.PORT || 8080,
-  host: env.HOST || '0.0.0.0',
-  get serverUrl() {
-    return `http://${this.host}:${this.port}`;
-  }
+    mongodbUri: 'mongodb://localhost:27017/quiz_assistant',
+    port: env.PORT || 8080,
+    host: env.HOST || '0.0.0.0',
+    get serverUrl() {
+        return `http://${this.host}:${this.port}`;
+    }
 };
 
 // REF:  https://github.com/jscomplete/learn-fullstack-javascript/
